5b06e7d1f1781ae3f2a55c434c24ea79daebd0bb
Vagner Fabricio <wagnerfabricio@hotmail.com>
2022-06-13 21:17:51 -0300

    feat: config aws with heroku

--- src/configs/s3.config.ts
+++ src/configs/s3.config.ts
@@ -4,7 +4,7 @@ const s3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
