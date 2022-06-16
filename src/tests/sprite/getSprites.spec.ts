0c5d87e5669ba1c5e6cfd0a4b2f22df4849ab15f
daniels <ddan.mm.16@gmail.com>
2022-06-16 11:23:58 -0300

    test: get sprites with token test working

---
+++ src/tests/sprite/getSprites.spec.ts
@@ -0,0 +1,33 @@
+import app from "../../app";
+import { connection } from "../../tests";
+import { generateToken, generateAdmin, generateNotAdmin } from ".";
+import supertest from "supertest";
+import { User } from "../../entities";
+import { userRepository } from "../../repositories";
+
+describe("Get all sprites", () => {
+  let userNotAdmin: User;
+
+  beforeAll(async () => {
+    await connection.connect();
+    userNotAdmin = await userRepository.save({
+      ...generateNotAdmin(),
+      adm: false,
+    });
+  });
+
+  afterAll(async () => {
+    await connection.disconnect();
+  });
+
+  it("Sprite as JSON response | Status code: 200", async () => {
+    const token = generateToken(userNotAdmin);
+
+    const response = await supertest(app)
+      .get("/api/sprites")
+      .set("Authorization", "Bearer " + token);
+
+    expect(response.status).toBe(200);
+    expect(typeof response.body).toBe("object");
+  });
+});
