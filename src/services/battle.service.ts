9910ca5b1a48d9b4834eb8e3e8c6833e1bc1f818
Diego <diegohpsousa@gmail.com>
2022-06-19 22:49:55 -0300

    feat/accuracy calc, hits calc, attack type scale, non-fight with yourself, lvl rate between lvl selected

--- src/services/battle.service.ts
+++ src/services/battle.service.ts
@@ -1,22 +1,20 @@
 import { sign, verify } from "jsonwebtoken";
-import { Attack, Char } from "../entities";
-import { IFighters } from "../interfaces";
+import { Char } from "../entities";
 import { charRepository as charRepo } from "../repositories";
 import { battleUtil as btu } from "../utils";
 import { Request } from "express";
+import { IFighters } from "../interfaces";
 import { AppError } from "../errors";
 
 class BattleService {
   start = async (char: Char, enemy_level: number) => {
     //randomize enemy
-    const enemy = await charRepo.getRandomChar(enemy_level);
-
-    //verify first attacker
-    if (enemy.status.agility > char.status.agility) {
-      return;
+    let enemy = await charRepo.getRandomChar(enemy_level);
+    while (enemy.id === char.id) {
+      enemy = await charRepo.getRandomChar(enemy_level);
     }
-    char.token = "";
 
+    char.token = "";
     const token: string = sign({ char, enemy }, process.env.SECRET_KEY, {
       expiresIn: process.env.BATTLE_EXPIRES_IN,
     });
@@ -28,14 +26,44 @@ class BattleService {
     });
   };
 
+  charAttack = async (attackId, fighters) => {
+    const { char, enemy } = fighters;
+    const charAttack = char.attacks.find((attack) => attack.id === attackId);
+    const damage = btu.damageCalculator(char, enemy, charAttack);
+    enemy.status.hp -= damage;
+    return {
+      damage: damage,
+      charAttack: charAttack,
+    };
+  };
+
+  enemyAttack = async (fighters: IFighters) => {
+    const { char, enemy } = fighters;
+    const enemyAttack = btu.enemyRandomAttack(fighters);
+    const enemyDamage = btu.damageCalculator(enemy, char, enemyAttack);
+    char.status.hp -= enemyDamage;
+    return {
+      enemyDamage: enemyDamage,
+      enemyAttack: enemyAttack,
+    };
+  };
+
   battle = async ({ body, fighters }: Request) => {
     const { char, enemy } = fighters;
 
-    const charAttack = char.attacks.find(
-      (attack) => attack.id === body.attackId
+    if (enemy.status.agility > char.status.agility) {
+      const { enemyAttack, enemyDamage } = await this.enemyAttack(fighters);
+      const { charAttack, damage } = await this.charAttack(
+        body.attackId,
+        fighters
+      );
+    }
+    const { charAttack, damage } = await this.charAttack(
+      body.attackId,
+      fighters
     );
-    const damage = btu.damageCalculator(fighters, charAttack);
-    enemy.status.hp -= damage;
+    const { enemyAttack, enemyDamage } = await this.enemyAttack(fighters);
+
     if (enemy.status.hp <= 0) {
       await charRepo.update(char.id, { token: null });
       return {
@@ -46,9 +74,6 @@ class BattleService {
         },
       };
     }
-    const enemyAttack = btu.enemyRandomAttack(fighters);
-    const enemyDamage = btu.damageCalculator(fighters, enemyAttack);
-    char.status.hp -= enemyDamage;
     if (char.status.hp <= 0) {
       await charRepo.update(char.id, { token: null });
       return {
@@ -65,27 +90,51 @@ class BattleService {
     });
     await charRepo.update(char.id, { token: updatedUserCharToken });
 
+    const msg =
+      enemy.status.agility > char.status.agility
+        ? {
+            first_attack: `${enemy.name} is faster than ${char.name} and attack first.`,
+            enemy_atatck:
+              enemyDamage === 0
+                ? `${enemy.name} missed ${enemyAttack.name}`
+                : `${enemy.name} used ${enemyAttack.name}`,
+            char_damage:
+              enemyDamage === 0
+                ? `${char.name} took no damage`
+                : `${char.name} take ${enemyDamage} damage`,
+            char_attack:
+              damage === 0
+                ? `${char.name} missed ${charAttack.name}`
+                : `${char.name} used ${charAttack.name}`,
+            enemy_damage:
+              damage === 0
+                ? `${enemy.name} took no damage`
+                : `${enemy.name} take ${damage} damage`,
+          }
+        : {
+            char_attack:
+              damage === 0
+                ? `${char.name} missed ${charAttack.name}`
+                : `${char.name} used ${charAttack.name}`,
+            enemy_damage:
+              damage === 0
+                ? `${enemy.name} took no damage`
+                : `${enemy.name} take ${damage} damage`,
+            enemy_atatck:
+              enemyDamage === 0
+                ? `${enemy.name} missed ${enemyAttack.name}`
+                : `${enemy.name} used ${enemyAttack.name}`,
+            char_damage:
+              enemyDamage === 0
+                ? `${char.name} took no damage`
+                : `${char.name} take ${enemyDamage} damage`,
+          };
+
     return {
-      msg: {
-        char_attack: `${char.name} used ${charAttack.name}`,
-        enemy_damage: `${enemy.name} take ${damage} damage`,
-        enemy_atatck: `${enemy.name} used ${enemyAttack.name}`,
-        char_damage: `${char.name} take ${enemyDamage} damage`,
-      },
+      msg,
       char,
       enemy,
     };
-  };
-
-  calculateDamage = async (playerChar: Char, player_atack_id: string) => {
-    const fighters = verify(
-      playerChar.token,
-      process.env.SECRET_KEY,
-      async (err, decoded) => {
-        return decoded;
-      }
-    );
-    const damage = 1;
   };
 }
 
